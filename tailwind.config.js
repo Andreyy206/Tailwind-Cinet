1c84833aa140f41589c473bbd21be84e4193fe70
sibayarn <mysibayaneduardandreimaruquez@gmail.com>
2024-10-10 15:57:54 +0800

    news50%

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,21 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        "light":"#ffffff",
-        "dark": "#2b2b2b",
-        "primary":"#005E7F",
-        "accent":"#ffc139",
+      colors: {
+        light: "#ffffff",
+        dark: "#2b2b2b",
+        primary: "#005E7F",
+        accent: "#ffc139",
       },
-      backgroundImage:{
-        "banner-desk":"url('/dist/img/cinet-banner-desktop.svg')",
-
+      backgroundImage: {
+        "banner-desk": "url('/dist/img/cinet-banner-desktop.svg')",
+        "categ-1": "url('/dist/IMG/categories-drama.jpg')",
+        "categ-2": "url('/dist/IMG/categories-cartoons.jpg')",
+        "categ-3": "url('/dist/IMG/categories-horror.jpg')",
+        "categ-4": "url('/dist/IMG/categories-sci-fi.jpg')",
+        "categ-5": "url('/dist/IMG/categories-comedy.jpg')",
       },
     },
   },
   plugins: [],
-}
+};
